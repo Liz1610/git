6ec672a689d19c680d21e06aa128c05604962e14
Samuel Camacho <samloza593@gmail.com>
2025-08-02 22:08:41 -0400

    loco5

--- prueba.py
+++ prueba.py
@@ -1,10 +1,4 @@
 print("Hello Git")
 print("cambios")
 print("hola Samuel")
-<<<<<<< HEAD
-print("hola Vania")
-print("Konichiwa :3!!!")
-=======
-resta=9-5
-print(resta)
->>>>>>> b905efe047e2f804498c72f8df36c87117fe6cdf
+
