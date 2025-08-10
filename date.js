90d9c2abad5f9d6554e2800ca1bb8f8d906fa4ff
Liz1610 <vaniarojasgomez1610@gmail.com>
2025-08-10 18:52:49 -0400

    b

---
+++ date.js
@@ -0,0 +1,5 @@
+const date = new Date();
+const year = date.getFullYear();
+const month = date.getMonth();
+const day = date.getDay();
+console.log(`${year}-${month}-${day}`);
--- prueba.py
+++ prueba.py
@@ -3,3 +3,8 @@ print("cambios")
 print("hola Samuel")
 resta=9-5
 print(resta)
+print("Konichiwa :3!!!")
+print(":D")
+suma=4+5
+print(suma)
+
